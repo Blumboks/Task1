ab90f00d7ccd99f8f6504f5092060f951aeee1a8
Dmitry <noisn@mail.ru>
2024-05-07 18:51:24 +0300

    add new task

--- Program.cs
+++ Program.cs
@@ -15,31 +15,48 @@
 // FromMToN(10, 21);
 
 
-Console.Write("Введите число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-AkermanFunction(m,n);
+// AkermanFunction(m, n);
 
-void AkermanFunction(int m, int n)
-{
-    Console.Write(Akerman(m, n)); 
-}
+// void AkermanFunction(int m, int n)
+// {
+//     Console.Write(Akerman(m, n));
+// }
 
-int Akerman(int m, int n)
-{
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0 && m > 0)
-    {
-        return Akerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akerman(m - 1, Akerman(m, n - 1)));
-    }
-}
+// int Akerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akerman(m - 1, Akerman(m, n - 1)));
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int[] array = { 5, 7, 9, 2, 88, 44 };
+//         PrintArrayReversed(array, array.Length - 1);
+//     }
+//     static void PrintArrayReversed(int[] array, int index)
+//     {
+//         if (index >= 0)
+//         {
+//             Console.Write(array[index] + " ");
+//             PrintArrayReversed(array, index - 1);
+//         }
+//     }
+// }
